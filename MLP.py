957c8212f816ed68dfc56814b712d60778b345af
OscarFlores-IFi <if715029@gmail.com>
2021-08-31 18:04:47 -0500

    First iteration works :)

--- MLP.py
+++ MLP.py
@@ -10,8 +10,6 @@ def MLP(X, Y, inner_layers=[]):
     def activation_function(x):
         return 1/(1+np.exp(-x)) # Sigmoid
         # return np.tanh(x) # Tanh
-
-    inner_layers = [2,3]
     
     ##### Define layers and weights #####
     layers = [X.shape[1],Y.shape[1]] # X, inner layer1, il2, ..., iln, Y 
@@ -19,24 +17,22 @@ def MLP(X, Y, inner_layers=[]):
         for i in range(len(inner_layers)):
             layers.insert(i+1,inner_layers[i])   
 
-    # MLPw = [np.ones((layers[i+1],layers[i]+1)) for i in range(len(layers)-1)]
-    MLPw = [np.random.rand(layers[i+1],layers[i]+1)*2-1 for i in range(len(layers)-1)]
+    # MLPw = [np.ones((layers[i+1],layers[i]+1)) for i in range(len(layers)-1)] # initializing weights at 0
+    MLPw = [np.random.rand(layers[i+1],layers[i]+1)*2-1 for i in range(len(layers)-1)] # initializing weights randomly
+    MLPx = [np.ones((layers[i]+1,X.shape[0])) for i in range(len(layers)-1)]
+    MLPy = [np.ones((layers[i+1],X.shape[0])) for i in range(len(layers)-1)]
+
+    MLPx[0][1:,:] = X.T 
+    for i in range(len(layers)-1): 
+        MLPy[i] = activation_function(np.matmul(MLPw[i],MLPx[i]))
+        try:
+            MLPx[i+1][1:,:] = MLPy[i]
+        except:
+            pass
     
-    ##### Calculation of outputs #####
-    nX = np.ones((X.shape[0],X.shape[1]+1))
-    nX[:,1:] = X 
-    
-    y1 = activation_function(np.matmul(MLPw[0],nX.T))
-    
-    # MLPx = [X.T
-    # MLPy = [np.ones((layers[i+1],1)) for i in range(len(layers)-1)]
-    
-    # def MLPy_act():
-    #     np.dot(MLPw[0],MLPy[0])
     print(time.time()-t0)
-    return y1
-    for layer in layers: 
-        pass
+    return MLPw, MLPx, MLPy
+ 
         
     
 
@@ -48,4 +44,4 @@ Y = pd.read_csv('deterministic.csv',index_col=0).iloc[:,-2:].values
 
 
 # MLP(X,Y) # No inner layers
-y1 = MLP(X,Y,[4,3]) # With inner layers
+MLPw, MLPx, MLPy = MLP(X,Y,[4,3]) # With inner layers
